84881f5c0eca383d239c98228ff08dcb6a48d0b4
Kolobaev-Nikita <kolobaevni@student.bmstu.ru>
2021-05-13 02:13:17 +0300

    Поменял библиотеку в select. Добавил вывод всех строк

--- select.py
+++ select.py
@@ -1,13 +1,12 @@
-from tkinter import messagebox
-
+import psycopg2
 
 def select(strng, type_q):
     con = connection()
     cur = con.cursor()
 
-    type_name = dictt('dictionary_type', 'NONE')
-    nam = dictt('dictionary_name', 'NONE')
-    mag = dictt('dictionary_mag', 'NONE')
+    #type_name = dictt('dictionary_type', 'NONE')
+    #nam = dictt('dictionary_name', 'NONE')
+    #mag = dictt('dictionary_mag', 'NONE')
 
     query = ''
     sel = ['find all', 'show all', 'show the whole table', 'show everything', 'find everything']
@@ -20,3 +19,11 @@ def select(strng, type_q):
 
     query_d = ''
     query_d += 'delete from Tech'
+    
+    #Вывести все в таблице
+    for i in range(len(strng)):
+        al+=strng[i]+' '
+    #al.rstrip()
+    #print(al)
+    if al =='show all ':
+        query+= "* FROM Tech"
